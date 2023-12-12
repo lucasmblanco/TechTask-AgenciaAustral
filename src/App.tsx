a5ec4563fe64084cd7277b04cd9b36a69d1d9853
Lucas <lucasmatiasblanco@outlook.com>
2023-12-11 21:56:49 -0300

    Added Ionic framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 // import { useState } from 'react'
 import './App.css';
 
+import { setupIonicReact, IonApp, IonContent } from '@ionic/react';
+import '@ionic/react/css/core.css';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+setupIonicReact({ mode: 'ios' });
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div></div>
+      <IonApp>
+        <IonContent className="ion-padding"></IonContent>
+      </IonApp>
     </>
   );
 }
